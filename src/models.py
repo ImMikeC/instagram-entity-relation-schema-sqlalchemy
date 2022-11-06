b4d640dd9758b16b5ded158803f7a8ca6f235b8b
ImMikeC <hebressus@gmail.com>
2022-11-06 02:00:32 +0000

    Setting 4 schema models

--- src/models.py
+++ src/models.py
@@ -1,38 +1,84 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
-Base = declarative_base()
+miModelo = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Photo(miModelo):
+    __tablename__ = 'photo'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    title = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    uploaded = Column(Date, nullable=False)
+    views = Column(Integer, nullable=False)
+    path = Column(String(250), nullable=False)  
+    owner_id = Column(Integer, ForeignKey('user.id'))
+    follower_id = Column(Integer, ForeignKey('follower.id'))
+    album_id = Column(Integer, ForeignKey('album.id'))
+
+
+class User(miModelo):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    u_name = Column(String(250), nullable=False)
+    u_email = Column(String(250), nullable=False)
+
+class Follower(miModelo):
+    __tablename__ = 'follower'
+
+    id = Column(Integer, primary_key=True)
+    f_name = Column(String(250), nullable=False)
+    f_email = Column(String(250), nullable=False)
+
+class Comment(miModelo):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(255), nullable=True)
+    photo_id = Column(Integer, ForeignKey('photo.id'),nullable=False)
+    uploaded_time = Column(Integer, ForeignKey('photo.uploaded'), nullable=False)
+    comment_follower_id = Column(Integer, ForeignKey('photo.follower_id'), nullable=False)
+
+class Album(miModelo):
+    __tablename__ = 'album'
+
+    id = Column(Integer, primary_key=True)
+    album_title = Column(String(255), ForeignKey('photo.title'), nullable=True)
+    album_description = Column(String(250), ForeignKey('photo.description'), nullable=False)
+    album_views = Column(Integer, ForeignKey('photo.views'), nullable=True)
+
+class Tag(miModelo):
+    __tablename__ = 'tag'
+
+    id = Column(Integer, primary_key=True)
+    photo_tag_id = Column(String(255), ForeignKey('photo.id'), nullable=True)
+    photo_title_id = Column(String(255), ForeignKey('photo.title'), nullable=True)
+
+class Location(miModelo):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True)
+    location_short_name = Column(String(255), nullable=True)
+    location_name = Column(String(255), ForeignKey('photo.location'), nullable=True)
+    location_name = Column(String(255), ForeignKey('photo.location'), nullable=True)
+    location_time = Column(Date, ForeignKey('photo.uploaded'), nullable=True)
+    
+
+
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+# Draw from SQLAlchemy base
 try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
+    result = render_er(miModelo, 'Diagrama_4.png')
+    print("Success! Check your new diagram file")
 except Exception as e:
-    print("There was a problem genering the diagram")
+    print("There was a problem genering your diagram")
     raise e
